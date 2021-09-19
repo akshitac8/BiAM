72a75340b764d620f26c9744607c1e22b9db65c9
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2021-09-19 17:45:31 +0530

    Update evaluate_nus.py
    Replaced **util** as **util_nus**

--- evaluate_nus.py
+++ evaluate_nus.py
@@ -13,7 +13,7 @@ import torch.optim as optim
 import torch.backends.cudnn as cudnn
 import torch.nn.functional as F
 import model as model
-import util as util
+import util_nus as util
 from config import opt
 import numpy as np
 import random
